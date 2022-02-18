fc81a7d4f6f33c71404984b9f2edd849cd6dd258
AugustoMagalhaes <augustommg@gmail.com>
2022-02-18 19:09:58 -0300

    R02: Criando o Component Solar System com o 'data-testid' indicado.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Header />
+        <SolarSystem />
         <p>Sistema Solar</p>
       </>
     );
---
+++ src/components/SolarSystem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">Solar</div>
+    );
+  }
+}
+
+export default SolarSystem;
