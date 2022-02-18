d3c3ef74cc49a77b7138443c49c32142782c4090
AugustoMagalhaes <augustommg@gmail.com>
2022-02-18 17:50:45 -0300

    R01: Criando o componente Header com uma tag 'header' e um 'h1' com o titulo da pagina.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <>
+        <Header />
+        <p>Sistema Solar</p>
+      </>
+    );
   }
 }
 
