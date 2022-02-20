a6c7b0a2adc1eaaff1351c15caf3787e522aa8fa
AugustoMagalhaes <augustommg@gmail.com>
2022-02-20 17:30:40 -0300

    R06: Implementando a renderizaçao da lista com os planetas do sistema solar.

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -5,7 +5,7 @@ class PlanetCard extends Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
+      <div data-testid="planet-card" className="planet-card">
         <p data-testid="planet-name">{planetName}</p>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system" className="solar-div">
         <Title headline="Planetas" />
+        <div className="planet-container">
+          {planets.map((planet) => (
+            <PlanetCard
+              key={ planet.name }
+              planetName={ planet.name }
+              planetImage={ planet.image }
+            />
+          ))}
+        </div>
       </div>
     );
   }
