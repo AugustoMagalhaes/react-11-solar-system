8bb7dcaea89c04d48d8b682977f02e47a43e029f
AugustoMagalhaes <augustommg@gmail.com>
2022-02-20 17:35:18 -0300

    R07: Implementando o Componente 'Missions'; Renderizando o Componente 'Missions' em 'App.js'.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 import SolarSystem from './components/SolarSystem';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <>
         <Header />
         <SolarSystem />
+        <Missions />
         <p>Sistema Solar</p>
       </>
     );
