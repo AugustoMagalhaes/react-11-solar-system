3f7b902bfee04103b841dc52ab3184871294fae9
AugustoMagalhaes <augustommg@gmail.com>
2022-02-21 11:12:40 -0300

    R10: Implementando a renderizaçao de 'MissionCard' no Componente 'Missions'.

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -5,11 +5,11 @@ class MissionCard extends Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div data-testid="mission-card">
-        <p data-testid="mission-name">{ name }</p>
-        <p data-testid="mission-year">{ year }</p>
-        <p data-testid="mission-country">{ country }</p>
-        <p data-testid="mission-destination">{ destination }</p>
+      <div data-testid="mission-card" className="mission">
+        <p data-testid="mission-name">{name}</p>
+        <p data-testid="mission-year">{year}</p>
+        <p data-testid="mission-country">{country}</p>
+        <p data-testid="mission-destination">{destination}</p>
       </div>
     );
   }
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions" className="missions-div">
         <Title headline="Missões" />
+        <div className="missions-container">
+          { missions.map((mission) => {
+            const { name, year, country, destination } = mission;
+            return (<MissionCard
+              key={ name }
+              name={ name }
+              year={ year }
+              country={ country }
+              destination={ destination }
+            />);
+          }) }
+        </div>
       </div>
     );
   }
